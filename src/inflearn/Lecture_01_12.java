bc63fb62d1d8d8a2287a791e512392ff7fe5b92b
Seohyun <tjgus9138@naver.com>
2025-03-05 20:48:48 +0900

    인프런 섹션2 문제풀이

--- src/inflearn/Lecture_01_12.java
+++ src/inflearn/Lecture_01_12.java
@@ -1,4 +1,33 @@
 package inflearn;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Lecture_01_12 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+        String line = in.readLine();
+        String input = "";
+//        String res = ""; // 결과 저장할 문자열
+        StringBuilder res = new StringBuilder();
+        int num;
+        for(int i = 0; i < n; i++) {
+//            String num2 = ""; // 2진수
+            StringBuilder num2 = new StringBuilder();
+            input = line.substring(i*7, i*7+7);
+            for(int j = 0; j < 7; j++) {
+                if(input.charAt(j) == '#') {
+                    num2.append('1');
+                } else{
+                    num2.append('0');
+                }
+            }
+            num = Integer.parseInt(num2.toString(), 2);
+            res.append((char)(num));
+        }
+        System.out.println(res);
+    }
 }
+
