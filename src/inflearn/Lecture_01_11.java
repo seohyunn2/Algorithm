c3de4cdfdf8bb2d2f4b2b4dea6c62156af2e1a5a
Seohyun <tjgus9138@naver.com>
2025-03-05 20:10:14 +0900

    인프런 섹션2 문제풀이

--- src/inflearn/Lecture_01_11.java
+++ src/inflearn/Lecture_01_11.java
@@ -3,34 +3,45 @@ package inflearn;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 public class Lecture_01_11 {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String line = in.readLine();
-        char[] input = line.toCharArray();
 
-        List<Character> res = new ArrayList<>();
+        char[] input = line.toCharArray();
+        int N = input.length;
+
+        Stack<Character> res = new Stack<>();
+
+        res.push(input[0]);
         int cnt = 1;
-        for(int i = 0; i < input.length; i++) {
-            if(i == 0) {
-                res.add(input[i]);
+        for(int i = 1; i < N; i++) {
+            if(i == N - 1) {
+                if((input[i] == res.peek())) {
+                    cnt++;
+                    res.push((char)(cnt+'0'));
+                } else {
+                    res.push((char)(cnt+'0'));
+                    res.push(input[i]);
+                }
             } else {
-                if(input[i] == input[i-1]) {
+                if (input[i] == res.peek()) {
                     cnt++;
                 } else {
-                    if(cnt>1) {
-//                        System.out.println(cnt);
-                        res.add((char) (cnt + '0'));
+                    if (cnt != 1) {
+//                        cnt++;
+                        res.push((char) (cnt + '0'));
+                        res.push(input[i]);
                         cnt = 1;
                     } else {
-                        res.add(input[i]);
+                        res.push(input[i]);
                     }
                 }
             }
         }
+
         for(int i = 0; i < res.size(); i++) {
             System.out.printf("%c", res.get(i));
         }
