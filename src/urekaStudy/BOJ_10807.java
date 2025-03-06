289074b3b39213ab5edb67a0ec37ac3c5dbdd8ee
Seohyun <tjgus9138@naver.com>
2025-03-06 17:27:49 +0900

    스터디 문제풀이

---
+++ src/urekaStudy/BOJ_10807.java
@@ -0,0 +1,31 @@
+package urekaStudy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10807 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(in.readLine(), " ");
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(in.readLine(), " ");
+        int v = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+        for(int i = 0; i < N; i++) {
+            if(arr[i] == v) {
+                cnt ++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
