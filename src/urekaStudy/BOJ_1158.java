5553dc9317e97a27901a3b086e9fdac70d4c0697
Seohyun <tjgus9138@naver.com>
2025-03-06 16:34:41 +0900

    스터디 문제풀이

---
+++ src/urekaStudy/BOJ_1158.java
@@ -0,0 +1,5 @@
+package urekaStudy;
+
+public class BOJ_1158 {
+
+}
--- src/urekaStudy/BOJ_1475.java
+++ src/urekaStudy/BOJ_1475.java
@@ -1,7 +1,5 @@
 package urekaStudy;
-
 import java.util.Scanner;
-
 public class BOJ_1475 {
     static int input, max, maxIdx, six, nine;
     static int[] num;
@@ -10,35 +8,51 @@ public class BOJ_1475 {
 
         input = sc.nextInt();
         num = new int[10];
-        while (input != 0) {
-            num[input % 10]++;
+        while(input != 0) {
+            num[input%10]++;
             input /= 10;
         }
         six = 0;
         nine = 0;
         max = Integer.MIN_VALUE;
         maxIdx = -1;
-        for (int i = 0; i < 10; i++) {
-            if (i == 6) {
+        for(int i = 0; i < 10; i++) {
+            if(i == 6) {
                 six = num[i];
-            } else if (i == 9) {
+            } else if(i==9) {
                 nine = num[i];
             } else {
-                if (max < num[i]) {
+                if(max < num[i]) {
                     max = num[i];
                     maxIdx = i;
                 }
             }
-
-            int sum = six + nine;
-            if (sum % 2 == 0) {
-                sum /= 2;
-            } else {
-                sum = sum / 2 + 1;
-            }
-            if (max > sum) System.out.println(max);
-            else System.out.println(sum);
+//             System.out.printf("update max : %d     max_idx = %d\n six = %d, nine = %d\n", max, maxIdx, six, nine);
         }
+        // 풀이 1 -> 오답
+        // max가 6, 9보다 큰 경우 -> 그대로 출력
+        // max가 6이나 9보다 작은 경우 -> 6, 9 값 더해서 나누기 2 (짝수면 그대로, 홀수면 +1 해서 출력)
 
+//        if(max > six && max > nine) {
+//            System.out.println(max);
+//        } else {
+//            int sum = six + nine;
+//            if(sum % 2 == 0) {
+//                System.out.println(sum/2);
+//            } else {
+//                System.out.println(sum/2 + 1);
+//            }
+//        }
+
+        int sum = six + nine;
+        if(sum %2 == 0) {
+            sum /= 2;
+        } else {
+            sum = sum / 2 + 1;
+        }
+        if(max > sum) System.out.println(max);
+        else System.out.println(sum);
     }
+
+
 }
