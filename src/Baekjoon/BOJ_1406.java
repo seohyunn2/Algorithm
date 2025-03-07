93ff18a8f81e8e5f90fdd4ccd75f4248f5171a7c
Seohyun <tjgus9138@naver.com>
2025-03-07 09:02:40 +0900

    백준 문제풀이

--- src/Baekjoon/BOJ_1406.java
+++ src/Baekjoon/BOJ_1406.java
@@ -17,8 +17,8 @@ public class BOJ_1406 {
 
         ListIterator<Character> iter = ll.listIterator();
         // 명령어 개수 입력받기
-        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
+//        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+        int n = Integer.parseInt(in.readLine());
 
         // 커서를 맨 뒤로 이동시켜주기 (문제 조건)
         while(iter.hasNext()) {
@@ -27,9 +27,11 @@ public class BOJ_1406 {
 
         // 명령어 입력받고 처리하기
         for(int i = 0; i < n; i++) {
-            st = new StringTokenizer(in.readLine(), " ");
-            char k = st.nextToken().charAt(0);
+//            st = new StringTokenizer(in.readLine(), " ");
+//            char k = st.nextToken().charAt(0);
+            String input = in.readLine();
 
+            char k = input.charAt(0);
             switch(k) {
                 case 'L':
                     if(iter.hasPrevious()) {
@@ -48,18 +50,27 @@ public class BOJ_1406 {
                     }
                     break;
                 case 'P':
-                    char c = st.nextToken().charAt(0);
+                    char c = input.charAt(2);
                     iter.add(c);
                     break;
             }
         }
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder s = new StringBuilder();
-        for(int i = 0; i < ll.size(); i++) {
-            s.append(ll.get(i));
+//        StringBuilder s = new StringBuilder();
+//        for(int i = 0; i < ll.size(); i++) {
+//            s.append(ll.get(i));
+//        }
+//        bw.write(s.toString());
+
+//        for(int i = 0; i < ll.size(); i++) {
+//            bw.write(ll.get(i));
+//        }
+
+        for(char f:ll) {
+            bw.write(f);
         }
-        bw.write(s.toString());
+
         bw.flush();
         bw.close();
     }
