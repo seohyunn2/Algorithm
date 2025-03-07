55fb6035aebc6f72eb922db5e98f846b3fa775b6
Seohyun <tjgus9138@naver.com>
2025-03-07 09:26:39 +0900

    백준 문제풀이

---
+++ src/Baekjoon/BOJ_18108.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class BOJ_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        System.out.println(n-543);
+    }
+}
