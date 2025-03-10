3f34dafac5379455f67ff6478a7d90da0503c03e
Seohyun <tjgus9138@naver.com>
2025-03-10 13:35:28 +0900

    백준 문제풀이

--- src/Baekjoon/BOJ_10811.java
+++ src/Baekjoon/BOJ_10811.java
@@ -1,6 +1,8 @@
 package Baekjoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class BOJ_10811 {
     public static void main(String[] args) {
@@ -9,24 +11,24 @@ public class BOJ_10811 {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] arr = new int[n];
+        Stack<Integer> rev = new Stack<>();
         for(int i = 0; i < n; i++) {
-            arr[i] = i;
+            arr[i] = i + 1;
         }
         for(int i = 0; i < m; i++) {
-            int s = sc.nextInt() - 1;
-            int f = sc.nextInt() - 1;
-            System.out.println("s : "+s+" f: "+f );
+            int s = sc.nextInt();
+            int f = sc.nextInt();
             if(s != f) {
-                for(int j = s; j <= f; j++, f--) {
-                    arr[j] = f;
+                for(int j = s; j <= f; j++) {
+                    rev.push(arr[j - 1]);
                 }
-                for(int k = 0; k < n; k++) {
-                    System.out.printf("%d ", arr[i]+1);
+                for(int k = s - 1; k <= f - 1; k++) {
+                    arr[k] = rev.pop();
                 }
             }
         }
         for(int i = 0; i < n; i++) {
-            System.out.printf("%d ", arr[i]+1);
+            System.out.print(arr[i] + " ");
         }
     }
 }
--- src/Baekjoon/BOJ_1546.java
+++ src/Baekjoon/BOJ_1546.java
@@ -1,4 +1,23 @@
 package Baekjoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class BOJ_1546 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double[] score = new double[n];
+        for(int i = 0; i < n; i++) {
+            score[i] = sc.nextInt();
+        }
+        Arrays.sort(score);
+        double maxScore = score[n-1];
+        double sum = 0;
+        for(int i = 0; i < n; i++) {
+            score[i] = score[i] / maxScore*100;
+            sum+=score[i];
+        }
+        System.out.println(sum/n);
+    }
 }
