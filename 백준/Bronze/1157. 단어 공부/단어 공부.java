a50a915489ed20cb6be6495e190f04700243d11f
이서현 <164132741+seohyunn2@users.noreply.github.com>
2025-03-11 15:28:02 +0900

    [Bronze I] Title: 단어 공부, Time: 256 ms, Memory: 23168 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] input = in.readLine().toUpperCase().toCharArray();
+        int[] alpha = new int[26];
+        for(int i = 0; i < input.length; i++) {
+            alpha[(int)(input[i]) - 65]++;
+        }
+
+        int max = Integer.MIN_VALUE;
+        char idx = 'a';
+        for(int i = 0; i < 26; i++) {
+            if(alpha[i] > max) {
+                max = alpha[i];
+                idx = (char)(i+65);
+            }
+        }
+        int cnt = 0;
+        for(int i = 0; i < 26; i++) {
+            if(alpha[i] == max) {
+                cnt++;
+            }
+        }
+        if (cnt > 1) {
+            System.out.println("?");
+        } else {
+            System.out.println(idx);
+        }
+    }
+}
