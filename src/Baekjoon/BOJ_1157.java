85b39f63ab0ab685f910d3a20297c75d7d9a42fe
이서현 <iseohyeon@iseohyeon-ui-MacBookAir.local>
2025-03-11 15:28:54 +0900

    백준 문제풀이

--- src/Baekjoon/BOJ_1157.java
+++ src/Baekjoon/BOJ_1157.java
@@ -3,6 +3,7 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class BOJ_1157 {
     public static void main(String[] args) throws IOException {
@@ -14,6 +15,24 @@ public class BOJ_1157 {
             alpha[(int)(input[i]) - 65]++;
         }
 
-
+        int max = Integer.MIN_VALUE;
+        char idx = 'a';
+        for(int i = 0; i < 26; i++) {
+            if(alpha[i] > max) {
+                max = alpha[i];
+                idx = (char)(i+65);
+            }
+        }
+        int cnt = 0;
+        for(int i = 0; i < 26; i++) {
+            if(alpha[i] == max) {
+                cnt++;
+            }
+        }
+        if (cnt > 1) {
+            System.out.println("?");
+        } else {
+            System.out.println(idx);
+        }
     }
 }
