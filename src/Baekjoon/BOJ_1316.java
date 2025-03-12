26494d43743dd637f90411666e4883757533180a
이서현 <iseohyeon@iseohyeon-ui-MacBookAir.local>
2025-03-12 19:17:22 +0900

    백준 문제풀이

--- src/Baekjoon/BOJ_1316.java
+++ src/Baekjoon/BOJ_1316.java
@@ -8,43 +8,14 @@ public class BOJ_1316 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int cnt = 0;
-        for(int i = 0; i < n; i++) {
+        int[] visited = new int[26]; // 지금 방문 중인 알파벳은 101, 다음 알파벳으로 넘어갈 때에는 이전 알파벳을 1로 바꿔줌.
 
+        for(int i = 0; i < n; i++) {
             boolean group = true;
             char[] word = sc.next().toCharArray();
-            int[] alpha = new int[26];
-            for(int j = 1; j < word.length; j++) {
-                if(j == 1) {
-                    if(word[0] != word[1]) {
-                        alpha[(int)(word[0]) - 97]++;
-                    }
-                } else if(j <= word.length - 1) {
-                    if(word[j] != word[j-1]) {
-                        alpha[(int)(word[j]) - 97]++;
-                        alpha[(int)(word[j-1]) - 97]++;
-                    }
-                }
-//                else if(j == word.length-1){
-//                    if(word[j] != word[j - 1]) {
-//                        alpha[(int)(word[j]) - 97]++;
-//                    }
-//                }
-                System.out.println(Arrays.toString(alpha));
+            boolean[] alpha = new boolean[26];
 
-            }
-            for(int k = 0; k < 26; k++) {
-                if(alpha[k] > 1) {
-                    group = false;
-                }
-            }
-            if(group) {
-                cnt++;
-            }
 
-            for(int a = 0; a < 26; a++) {
-                alpha[a] = 0;
-            }
         }
-        System.out.println(cnt);
     }
 }
