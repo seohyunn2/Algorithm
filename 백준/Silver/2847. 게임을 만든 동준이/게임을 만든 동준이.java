8998e8e87a724eb71564f6954282067066ad1a44
이서현 <164132741+seohyunn2@users.noreply.github.com>
2025-03-12 19:08:08 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 176 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+// 5 3 7 5
+// 2 3 7 5
+
+// 5 5 5
+// 4 5 5
+// 4 4 5
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ans = 0;
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = n-2; i >= 0; i--) {
+            if(arr[i] >= arr[i+1]) {
+                int diff = arr[i] - arr[i+1] + 1;
+                ans += diff;
+                arr[i] -= diff;
+            }
+        }
+        System.out.println(ans);
+    }
+}
