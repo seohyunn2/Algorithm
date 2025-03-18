ad13b6549d5cdb5a9117be9364e6faa5e0bf9c5b
이서현 <164132741+seohyunn2@users.noreply.github.com>
2025-03-18 20:21:37 +0900

    [Silver IV] Title: 제로, Time: 696 ms, Memory: 112476 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < n; i++) {
+            int k = sc.nextInt();
+            if(k == 0) {
+                stack.pop();
+            } else {
+                stack.push(k);
+            }
+        }
+//        System.out.println(stack);
+        int sum = 0;
+        if(stack.isEmpty()) {
+            System.out.println(0);
+        } else {
+            int s = stack.size();
+            for(int i = 0; i < s; i++) {
+                sum += stack.pop();
+            }
+            System.out.println(sum);
+        }
+    }
+}
