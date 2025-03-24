1f94316a461cb88583f69a80a50fa7da00207c0f
이서현 <164132741+seohyunn2@users.noreply.github.com>
2025-03-24 09:47:42 +0900

    [Silver IV] Title: 큐, Time: 380 ms, Memory: 35208 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,62 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Deque<Integer> q = new ArrayDeque<>();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i = 0; i < n; i++) {
+            String s = sc.next();
+            switch(s) {
+                case "push":
+                    q.offer(sc.nextInt());
+                    break;
+                case "pop":
+                    if(q.isEmpty()) {
+                        bw.write("-1");
+                    } else {
+                        bw.write(String.valueOf(q.poll()));
+                    }
+                    bw.write("\n");
+                    break;
+                case "size":
+                    bw.write(String.valueOf(q.size()) + "\n");
+                    break;
+                case "empty":
+                    if (q.isEmpty()) {
+                        bw.write("1");
+                    } else {
+                        bw.write("0");
+                    }
+                    bw.write("\n");
+                    break;
+                case "front":
+                    if (q.isEmpty()) {
+                        bw.write("-1");
+                    } else {
+                        bw.write(String.valueOf(q.peekFirst()));
+                    }
+                    bw.write("\n");
+                    break;
+                case "back":
+                    if (q.isEmpty()) {
+                        bw.write("-1");
+                    } else {
+                        bw.write(String.valueOf(q.peekLast()));
+                    }
+                    bw.write("\n");
+                    break;
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
