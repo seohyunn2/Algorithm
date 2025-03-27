11eba11d92ce199b42df3d23201ca6c5ace2e056
seohyunn2 <tjgus9138@naver.com>
2025-03-27 10:36:37 +0900

    백준 문제풀이

--- src/Baekjoon/BOJ_1316.java
+++ src/Baekjoon/BOJ_1316.java
@@ -8,14 +8,29 @@ public class BOJ_1316 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int cnt = 0;
-        int[] visited = new int[26]; // 지금 방문 중인 알파벳은 101, 다음 알파벳으로 넘어갈 때에는 이전 알파벳을 1로 바꿔줌.
-
+        int groupCnt = 0;
         for(int i = 0; i < n; i++) {
+            int[] visited = new int[26]; // 지금 방문 중인 알파벳은 2, 다음 알파벳으로 넘어갈 때에는 이전 알파벳을 1로 바꿔줌.
             boolean group = true;
             char[] word = sc.next().toCharArray();
-            boolean[] alpha = new boolean[26];
-
+            for(int j = 0; j < word.length; j++) {
+                if(visited[(int)(word[j] - 'a')] == 1) {
+                    group = false;
+                }
+                else {
+                    if(j != word.length-1 && word[j+1] == word[j]) {
+                        visited[(int)(word[j] - 'a')] = 2;
+                    } else {
+                        visited[(int)(word[j] - 'a')] = 1;
+                    }
+                }
+            }
+            if(group) {
+//                System.out.println(word);
+                groupCnt++;
+            }
 
         }
+        System.out.println(groupCnt);
     }
 }
---
+++ src/Baekjoon/BOJ_2738.java
@@ -0,0 +1,32 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class BOJ_2738 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] mat1 = new int[n][m];
+        int[][] mat2 = new int[n][m];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                mat1[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                mat2[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                System.out.print(mat1[i][j] + mat2[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
