d54ae0a16280433b042947840625c053d88cf15c
이서현 <164132741+seohyunn2@users.noreply.github.com>
2025-03-31 20:13:24 +0900

    [Silver III] Title: 회전하는 큐, Time: 172 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -0,0 +1,65 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        LinkedList<Integer> deque = new LinkedList<Integer>();
+
+        int cnt = 0;
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            deque.offer(i);
+        }
+
+        int[] arr = new int[m];	//  뽑으려는 수
+
+        for(int i = 0; i < m; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+        for(int i = 0; i < m; i++) {
+
+            // 덱에서 뽑으려는 숫자의 인덱스 찾기
+            int target = deque.indexOf(arr[i]);
+            int half;
+
+            // 짝수개면 인덱스 -1
+            if(deque.size() % 2 == 0) {
+                half = deque.size() / 2 - 1;
+            }
+            else {
+                half = deque.size() / 2;
+            }
+
+
+            // 중앙보다 앞에 있을 때
+            if(target <= half) {
+                // 앞에서 뽑아서 뒤로 보내
+                for(int j = 0; j < target; j++) {
+                    int temp = deque.pollFirst();
+                    deque.offerLast(temp);
+                    cnt++;
+                }
+            }
+            else {	// 중앙보다 뒤에 있을 때
+                //  뒤에서 뽑아서 앞으로 보내
+                for(int j = 0; j < deque.size() - target; j++) {
+                    int temp = deque.pollLast();
+                    deque.offerFirst(temp);
+                    cnt++;
+                }
+
+            }
+            deque.pollFirst();
+        }
+
+        System.out.println(cnt);
+
+    }
+}
